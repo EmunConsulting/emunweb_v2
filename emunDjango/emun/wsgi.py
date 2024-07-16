a53de16ad17c41cec35df3c40d1bca96b9e2d660
EmunConsulting <emunconsultingltd@gmail.com>
2024-07-16 17:43:07 +0300

    Update wsgi.py
    add whitenoise to wisgi file

--- emunDjango/emun/wsgi.py
+++ emunDjango/emun/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
+from whitenoise import WhiteNoise
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'emun.settings')
 
 application = get_wsgi_application()
+
+application = WhiteNoise(application, root='/home/xegai/emunweb/emunweb_v2/emunDjango/static/')
+
+
